f9ce9638fc1e1f440b48fd3010b00b8cccd08f57
Antoine <bababouch07@gmail.com>
2020-08-19 14:45:03 +0200

    Initial Commit

---
+++ SwiftUI Snake/SwiftUI_SnakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SnakeApp.swift
+//  SwiftUI Snake
+//
+//  Created by Antoine on 19/08/2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SnakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
